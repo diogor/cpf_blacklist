0e8e4ae68a9f0526bc76ca65f784ae7414a4b72e
Diogo Lima <diogo@dr6.com.br>
2021-02-06 15:27:53 -0300

    comando de import

--- blacklist/management/commands/import_list.py
+++ blacklist/management/commands/import_list.py
@@ -1,5 +1,9 @@
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from django.core.management.base import BaseCommand
 from django.utils.translation import gettext_lazy as _
+from blacklist.models import ListEntry
+from blacklist.validators import validate_CPF
 
 
 class Command(BaseCommand):
@@ -9,6 +13,29 @@ class Command(BaseCommand):
         parser.add_argument('file', type=str)
 
     def handle(self, *args, **options):
-        if options['file']:
-            self.stdout.write(self.style.WARNING(f'{options["file"]}'))
-        self.stdout.write(self.style.SUCCESS(_("Imported succefully!")))
+        b_list = options['file']
+        try:
+            lines = []
+            with open(b_list, 'r') as r:
+                lines = r.readlines()
+
+        except FileNotFoundError:
+            self.stdout.write(
+                self.style.ERROR(f'File not found. ({b_list})')
+            )
+        for cpf in lines:
+            cpf = cpf.strip("\n")
+            try:
+                validate_CPF(cpf)
+                ListEntry.objects.create(cpf=cpf)
+                self.stdout.write(
+                    self.style.SUCCESS(f'Imported ({cpf}).')
+                )
+            except IntegrityError:
+                self.stdout.write(
+                    self.style.WARNING(f'{cpf}: Already in list.')
+                )
+            except ValidationError as e:
+                self.stdout.write(
+                    self.style.ERROR(f'{cpf}: {",".join(e)}')
+                )
