b89cc0ecaa0011847d712124b77f5cc8694e97bf
Diogo Lima <diogo@dr6.com.br>
2021-02-06 13:50:07 -0300

    Ajeitando validator para aceitar somente números

--- blacklist/validators.py
+++ blacklist/validators.py
@@ -1,4 +1,3 @@
-import re
 from django.core.validators import EMPTY_VALUES
 from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
@@ -6,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 cpf_error_messages = {
     'invalid': _("Invalid CPF."),
-    'digits_only': _("Only numbers and separators are allowed."),
+    'digits_only': _("Only numbers are allowed."),
     'max_digits': _("This field requires 11 digits."),
 }
 
@@ -19,30 +18,34 @@ def dv_maker(v):
 
 def validate_CPF(value):
     """
-    Value can be either a string in the format XXX.XXX.XXX-XX or an
-    11-digit number.
+    Value can be a 11-digit number.
     """
 
     if value in EMPTY_VALUES:
         return u''
-    if not value.isdigit():
-        value = re.sub("[-\.]", "", value)
-    orig_value = value[:]
-    try:
-        int(value)
-    except ValueError:
-        raise ValidationError(cpf_error_messages['digits_only'])
+
     if len(value) != 11:
         raise ValidationError(cpf_error_messages['max_digits'])
-    orig_dv = value[-2:]
 
-    new_1dv = sum([i * int(value[idx]) for idx, i in enumerate(range(10, 1, -1))])
+    if not value.isdigit():
+        raise ValidationError(cpf_error_messages['digits_only'])
+
+    if len(set(list(value))) == 1:
+        raise ValidationError(cpf_error_messages['invalid'])
+
+    orig_dv = value[-2:]
+    new_1dv = sum([i * int(value[idx]) for idx, i
+                   in enumerate(range(10, 1, -1))])
+
     new_1dv = dv_maker(new_1dv % 11)
     value = value[:-2] + str(new_1dv) + value[-1]
-    new_2dv = sum([i * int(value[idx]) for idx, i in enumerate(range(11, 1, -1))])
+    new_2dv = sum([i * int(value[idx]) for idx, i
+                   in enumerate(range(11, 1, -1))])
+
     new_2dv = dv_maker(new_2dv % 11)
     value = value[:-1] + str(new_2dv)
+
     if value[-2:] != orig_dv:
         raise ValidationError(cpf_error_messages['invalid'])
 
-    return orig_value
+    return value
